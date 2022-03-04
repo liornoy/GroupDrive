f297e7675e8c6947c01ec62f18a95d8fe7acd60e
Lior Noy <ironoy@gmail.com>
2022-03-04 11:11:42 +0200

    push it

--- app/src/main/java/com/example/groupdrive/recyclerAdapter.java
+++ app/src/main/java/com/example/groupdrive/recyclerAdapter.java
@@ -1,8 +1,10 @@
 package com.example.groupdrive;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,24 +15,41 @@ import com.example.groupdrive.model.trip.Trip;
 import java.util.ArrayList;
 
 public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyViewHolder> {
-    private ArrayList<Trip>tripList;
-    public recyclerAdapter(ArrayList<Trip>tripsList) {
+    private ArrayList<Trip> tripList;
+
+    public recyclerAdapter(ArrayList<Trip> tripsList) {
         this.tripList = tripsList;
     }
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView tripTitleText;
         private TextView tripDurationText;
-        public MyViewHolder(final View view){
+        private Button joinLiveTripBtn;
+
+        public MyViewHolder(final View view) {
             super(view);
             tripTitleText = view.findViewById(R.id.TripNameTextView2);
             tripDurationText = view.findViewById(R.id.tripduration2);
+            joinLiveTripBtn = view.findViewById(R.id.button3);
+            joinLiveTripBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    gotoMaps(view);
+                }
+            });
         }
     }
+
     @NonNull
     @Override
     public recyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.trip_item,parent,false);
-        return  new MyViewHolder(itemView);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.trip_item, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    private void gotoMaps(View view) {
+        Intent switchActivityIntent = new Intent(view.getContext(), MapsActivity.class);
+        view.getContext().startActivity(switchActivityIntent);
     }
 
     @Override
@@ -39,7 +58,6 @@ public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyView
         String duration = tripList.get(position).getDuration();
         holder.tripTitleText.setText(name);
         holder.tripDurationText.setText(duration);
-
     }
 
     @Override
