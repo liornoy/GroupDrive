60664b0035dccaa8353e5846ee9dc4d1e6a6bfda
Ofir Levy <ofirly@mta.ac.il>
2022-03-27 22:20:05 +0300

    NOT WORKING YET - attempt to get GPS location from device

--- app/src/main/java/com/example/groupdrive/ui/fragments/MapsActivity.java
+++ app/src/main/java/com/example/groupdrive/ui/fragments/MapsActivity.java
@@ -2,6 +2,10 @@ package com.example.groupdrive.ui.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.example.groupdrive.R;
@@ -12,7 +16,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener
 {
 
 
@@ -35,10 +39,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap)
     {
         //coordinates are of the academic college, tel aviv
-        LatLng MTA = new LatLng(32.047565, 34.760741);
+        LatLng mta = new LatLng(32.047565, 34.760741);
         googleMap.addMarker(new MarkerOptions()
-                .position(MTA)
+                .position(mta)
                 .title("Marker in MTA"));
+
+        //attempting to get location
+
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 5000, 10, this);
+
+        //attempting to get location
+
+    }
+
+    @Override
+    public void onLocationChanged(Location loc)
+    {
+
     }
 
 }
