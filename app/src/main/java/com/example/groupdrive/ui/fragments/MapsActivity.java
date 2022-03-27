774c3019d5a96dd90af89125bafd7aaa34050bcd
Ofir Levy <ofirly@mta.ac.il>
2022-03-27 21:51:17 +0300

    added a marker to MTA when the map loads

--- app/src/main/java/com/example/groupdrive/ui/fragments/MapsActivity.java
+++ app/src/main/java/com/example/groupdrive/ui/fragments/MapsActivity.java
@@ -8,18 +8,37 @@ import com.example.groupdrive.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity  {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback
+{
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         getSupportActionBar().hide();
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        //the object that will be called when the map is ready. doesn't have to be this.
+        //for now just for simplicity purposes
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        //coordinates are of the academic college, tel aviv
+        LatLng MTA = new LatLng(32.047565, 34.760741);
+        googleMap.addMarker(new MarkerOptions()
+                .position(MTA)
+                .title("Marker in MTA"));
+    }
 
 }
