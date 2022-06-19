596ebc3c922d5d1b650b906ccbffbd60ba537824
liornoy <ironoy@gmail.com>
2022-06-19 16:41:52 +0300

    Add view trip details activity
    Added a new activity for viewing trip's details.
    From viewing the trip's details page, the user will be
    able to enter the creator's page.

---
+++ app/src/main/java/com/example/groupdrive/ui/fragments/ViewTripDetails.java
@@ -0,0 +1,30 @@
+package com.example.groupdrive.ui.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.groupdrive.R;
+
+public class ViewTripDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_trip_details);
+        TextView title_tv = findViewById(R.id.details_trip_title);
+        TextView description_tv = findViewById(R.id.details_trip_desc);
+        TextView date_tv = findViewById(R.id.details_trip_date);
+        TextView creator_tv = findViewById(R.id.details_trip_creator);
+        TextView location_tv = findViewById(R.id.details_meeting_point);
+        title_tv.setText(getIntent().getExtras().getString("tripTitle"));
+        description_tv.setText(getIntent().getExtras().getString("tripDesc"));
+        date_tv.setText(getIntent().getExtras().getString("tripDate"));
+        location_tv.setText(getIntent().getExtras().getString("tripLocation"));
+        creator_tv.setText(getIntent().getExtras().getString("tripCreator"));
+        Button creator_page_btn = findViewById(R.id.creator_page_btn);
+
+    }
+}
