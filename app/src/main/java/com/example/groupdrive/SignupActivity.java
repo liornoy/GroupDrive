8d09e62ea9db845366b77f25e69a7c0e79678a0b
liornoy <ironoy@gmail.com>
2022-07-02 16:48:30 +0300

    Split Sign-in and Sign-up pages
    Created a new Sign-up activity and refactored the sign-in
    to include a button for creating new user

--- app/src/main/java/com/example/groupdrive/SignupActivity.java
+++ app/src/main/java/com/example/groupdrive/SignupActivity.java
@@ -1,56 +1,51 @@
 package com.example.groupdrive;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.groupdrive.api.ApiClient;
 import com.example.groupdrive.api.ApiInterface;
 import com.example.groupdrive.ui.fragments.TripsActivity;
-
 
 import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
-    private Button sign_in_button;
-    private EditText sign_in_username, sign_in_password;
-
+public class SignupActivity extends AppCompatActivity {
+    private Button sign_up_button;
+    private EditText sign_up_username, sign_up_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        sign_in_button = findViewById(R.id.signInBtn);
-        sign_in_username = findViewById(R.id.oldUsernameText);
-        sign_in_password = findViewById(R.id.oldPasswordText);
-
-        sign_in_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onSignIn(view);
-            }
-        });
+        setContentView(R.layout.activity_signup);
+        sign_up_button = findViewById(R.id.signup_btn);
+        sign_up_username = findViewById(R.id.signup_username);
+        sign_up_password = findViewById(R.id.signup_password);
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  onSignUp();
+              }
+          }
+        );
     }
+
     public void onClick(View v) {
-        Intent switchActivityIntent = new Intent(this, SignupActivity.class);
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
         startActivity(switchActivityIntent);
     }
-    private void onSignIn(View view){
+
+    private void onSignUp(){
         ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
-        String username = sign_in_username.getText().toString();
-        String password = sign_in_password.getText().toString();
-        String requestURL = "/api/users/sign-in/"+username+"/"+password;
+        String username = sign_up_username.getText().toString();
+        String password = sign_up_password.getText().toString();
+        String requestURL = "/api/users/sign-up/"+username+"/"+password;
         Call<String> call;
         call = apiInterface.signIn(requestURL);
         Response<String> response = null;
