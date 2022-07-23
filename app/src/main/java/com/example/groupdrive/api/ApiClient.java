6e0504300ae4cac54843af9312eb80f7278ef069
liornoy <ironoy@gmail.com>
2022-07-23 17:53:37 +0300

    fix cropping

--- app/src/main/java/com/example/groupdrive/api/ApiClient.java
+++ app/src/main/java/com/example/groupdrive/api/ApiClient.java
@@ -18,7 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class ApiClient {
-    private static final String BASE_URL = "http://5.29.145.58:5000/";
+    private static final String BASE_URL = "http://192.168.14.53:5000/";
     private static Retrofit retrofit;
 
     public static Retrofit getApiClient() {
